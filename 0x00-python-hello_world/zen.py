23f6c2b64006d238f12dbf8bbe1fb34ca471fbaf
Mulubi <mulubiwarren@gmai.com>
2022-08-22 15:54:33 +0000

    Qn9

--- 0x00-python-hello_world/zen.py
+++ 0x00-python-hello_world/zen.py
@@ -21,4 +21,4 @@ If the implementation is hard to explain, it's a bad idea.\
 If the implementation is easy to explain, it may be a good idea.\
 Namespaces are one honking great idea -- let's do more of those!"
 
-print(str[0:98])
+print(str[:<98])
