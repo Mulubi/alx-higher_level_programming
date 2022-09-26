eb37baed902dbcbac9e09dec051b1eb198545dff
Mulubi <mulubiwarren@gmail.com>
2022-09-26 19:39:26 +0000

    Qn1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A class MyList that inherits from list"""
+
+class MyList(list):
+	pass
+
+def print_sorted(self):
+	"""prints the list, but sorted in ascending sort"""
+	print(MyList.sort())
