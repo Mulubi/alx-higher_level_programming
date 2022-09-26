46c4614db7ce3fc1dc54388f527b7ac1e284cb50
Mulubi <mulubiwarren@gmail.com>
2022-09-26 19:28:19 +0000

    Debugged Qn0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""A function that prints list of available
-attributes and methods of an object"""
+"""A function that prints list of available"\ 
+"attributes and methods of an object"""
 
 def lookup(obj):
 	"""Returns a list object """
