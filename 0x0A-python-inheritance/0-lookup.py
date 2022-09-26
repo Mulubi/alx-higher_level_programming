949e4b80da817c87073fa40f811cdd8fe9f11553
Mulubi <mulubiwarren@gmail.com>
2022-09-26 19:25:27 +0000

    Debugged Qn0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
+"""A function that prints list of available
+attributes and methods of an object"""
 
-class MyClass1(object):
-
-#Class Attributes
-	one = 'first'
-	two = 'second'
-
-	''' function that returns the list of available attributes and methods of an object '''
-
-	def __init__(self, my_attr1):
-		self.my_attr1 = my_attr1
-
-	def lookup(obj):
-		print(self.one, self.two, self.my_attr1)
-	
-	def my_meth(self):
-		pass
-
-m = MyClass1(2)
-m.lookup()
-
-#using __dict__ to access attributes of the objects
-print(m.__dict__.keys())
+def lookup(obj):
+	"""Returns a list object """
+	return (dir(obj))
