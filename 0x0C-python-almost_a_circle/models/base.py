b014bf8c0f14b2627903a6bab51191eedc2c7faf
Warren Wice <mulubiwarren@gmail.com>
2022-10-04 09:02:10 +0000

    DEbug Qn1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,11 +3,6 @@ class Base:
     __nb_objects = 0
     def __init__(self, id=None):
         self.name = name
-        self.id=None = id
+        self.id = id
         self.__nb_objects = nb_objects
     __nb_objects += 1
-
-    if id != None:
-        self.id = id=None
-    else:
-        self.id = __nb_objects
