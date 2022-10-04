abf8a0ca8fd95b96a4051fc0aaf751797b995ab2
Warren Wice <mulubiwarren@gmail.com>
2022-10-04 09:10:58 +0000

    Debug Qn1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 class Base:
+    """Base Model.
+    
+    This represents the base for all the other classes in this project
+    
+    Private Class Attributes:
+    __nb_objects (Int) only available in this Base Model"""
+
     __nb_objects = 0
     def __init__(self, id=None):
         self.name = name
         self.id = id
         self.__nb_objects = nb_objects
-    __nb_objects += 1
+
+    if id is not None:
+        self.id = None
+    else:
+        __nb_objects += 1
+        self.id = __nb_objects
