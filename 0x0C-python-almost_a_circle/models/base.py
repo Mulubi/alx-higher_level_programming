c4ab4cd6d1e83ecc948ef4a74448be94ade70ec5
Warren Wice <mulubiwarren@gmail.com>
2022-10-04 09:15:50 +0000

    Debug Qn1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
+
+"""Defines a base model class"""
 class Base:
     """Base Model.
     
     This represents the base for all the other classes in this project
     
     Private Class Attributes:
-    __nb_objects (Int) only available in this Base Model"""
+    __nb_objects (Int): number of instantiated Bases"""
 
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """Initializes a new Base
+        
+        Args:
+            id (int): The id of every new base
+        """
+
         self.name = name
         self.id = id
-        self.__nb_objects = nb_objects
-
+    
     if id is not None:
-        self.id = None
+        self.id = id
     else:
-        __nb_objects += 1
-        self.id = __nb_objects
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
