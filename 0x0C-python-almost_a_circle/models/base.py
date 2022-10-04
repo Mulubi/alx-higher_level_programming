9c1747a89ea488e9d117f4d177073d30ea551372
Warren Wice <mulubiwarren@gmail.com>
2022-10-04 08:48:46 +0000

    Qn1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        self.name = name
+        self.id=None = id
+        self.__nb_objects = nb_objects
+    __nb_objects += 1
+
+    if id != None:
+        self.id = id=None
+    else:
+        self.id = __nb_objects
