c127df3793f9ba1bba4dbc1c2ca6f550a9f7d4b3
Mulubi <mulubiwarren@gmail.com>
2022-12-06 04:18:29 -0800

    Debug QN0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,26 +5,32 @@ import sys
 
 
 def show_all():
-    # Get MySQL username, password and database from the user input
+    '''
+    Get MySQL username, password and database from the user input
+    '''
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    host = 'localhost'
+    port = 3306
 
-    # A way to connect to the database given
-    conn = MySQLdb.connect(host='localhost', port=3306, user=username,
+    ''' A way to connect to the database given '''
+    conn = MySQLdb.connect(host=host, port=port, user=username,
                            password=password, db=database)
 
-    # A cursor object
+    ''' A cursor object '''
     cursor = conn.cursor()
 
-    # execute a SELECT statement that gets all states from database
+    '''
+    execute a SELECT statement that gets all states from database
+    '''
     cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
 
-    # Print the results found
+    ''' Print the results found '''
     for row in cursor:
         print(row)
 
-    # Close the connection
+    ''' Close the connection '''
     conn.close()
 
 
