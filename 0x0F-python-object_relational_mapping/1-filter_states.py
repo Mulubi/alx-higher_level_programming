c5c1e876b52a86c4a21ddcb5556233466a912602
Mulubi <mulubiwarren@gmail.com>
2022-12-06 02:25:06 -0800

    Qn0

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,30 @@
+#!/usr/bin/python3
+
+import MySQLdb
 import sys
 
+# This is used to get the MySQL username, password and database from the user input
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+# A way to connect to the database given
+conn = MySQLdb.connect(host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        db=database)
+
+# A cursor object
+cursor = conn.cursor()
+
+# execute a SELECT statement that gets all states from the database
+cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+
+# Print the results found
+print('ID\tName')
+for row in cursor:
+    print(f'{row[0]}\{row[1]}')
+
+# Close the connection
+conn.close()
