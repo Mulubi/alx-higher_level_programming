19b48198623aec219bf1b4ba9cacd7142c7fe7c7
Warren Wice <mulubiwarren@gmail.com>
2022-12-06 10:36:07 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,17 +3,17 @@
 import MySQLdb
 import sys
 
-# This is used to get the MySQL username, password and database from the user input
+# Get the MySQL username, password and database from the user input
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
 # A way to connect to the database given
 conn = MySQLdb.connect(host='localhost',
-        port=3306,
-        user=username,
-        password=password,
-        db=database)
+                port=3306,
+                user=username,
+                password=password,
+                db=database)
 
 # A cursor object
 cursor = conn.cursor()
@@ -24,7 +24,7 @@ cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
 # Print the results found
 print('ID\tName')
 for row in cursor:
-    print(f'{row[0]}\{row[1]}')
+    print(f'{row[0]}\ {row[1]}')
 
 # Close the connection
 conn.close()
