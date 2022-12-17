9a54b51fe39ce476b1fc502a506cf3e852fe4708
Mulubi <mulubiwarren@gmail.com>
2022-12-17 02:41:00 -0800

    Qn2 debug

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -5,11 +5,12 @@ url=$1
 
 # Use curl to send a GET request to the URL
 # Store the status code in a variable
-status=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+status=$(curl -sL -o /dev/null -w "%{http_code}" "$url")
 
 # Check the status code
 if [ $status -eq 200 ]; then
-	  # If the status code is 200, display the body of the response
-	    response=$(curl -s "$url")
-	      echo "$response"
+	# If the status code is 200, display the body of the
+	# response
+	response=$(curl -sL "$url")
+	echo "$response"
 fi
