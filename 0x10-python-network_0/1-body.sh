612d21528317132c514b5596c63c52144bf4a83a
Warren Wice <mulubiwarren@gmail.com>
2022-12-17 12:31:43 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# Take in the URL as an argument
+url=$1
+
+# Use curl to send a GET request to the URL
+# Store the code in a response
+response=$(curl -s -o /dev/null -w "%{http_code}" $url)
+
+# Check the status code of the response
+if [ $response -eq 200 ]; then
+  # if the status code is 200, dis[lay the body response
+  curl -s $url
+fi
