cb6cf81e3096e9133fddaf51bf4a4c7c7faf4b76
Warren Wice <mulubiwarren@gmail.com>
2022-12-20 14:15:54 +0000

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# This bash script takes in URL and displays
-# all HTTP methods that the server will accept
-
-# Store the URL in a variable
-url=$1
-
-# Send an OPTIONS request to the server and store the response
-response=$(curl -X OPTIONS -s "$url")
-
-# Extract the Allow header from the response
-allow_header=$(echo "$response" | grep -i 'allow:')
-
-# Extract the list of allowed methods from the Allow header
-allowed_methods=$(echo "$allow_header" | awk -F':' '{print $2}' | tr -d ' ')
-
-# Display the list of allowed methods
-echo "Allowed methods: $allowed_methods"
+# Displays all HTTP methods the server will accept.
+curl -s -I "${1}" | grep "^Allow: .*" | cut -d " " -f 2-
