15af9c6f2bd978050d3bc112fab67ba80c65de82
Mulubi <mulubiwarren@gmail.com>
2022-12-20 06:04:44 -0800

    CHanges

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Send a DELETE request to a given URL and display the response body.
-curl -sX DELETE "$1"
+# This bash script sends a DELETE request to the URL
+# passed as the first argument and displays the body
+# of the response
+
+# Store the URL in a variable
+url=$1
+
+# Send a DELETE request and store the response
+response=$(curl -X DELETE -s "$url")
+
+# Display the body of the response
+echo "$response"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# This bash script takes in URL and displays
+# all HTTP methods that the server will accept
+
+# Store the URL in a variable
+url=$1
+
+# Send an OPTIONS request to the server and store the response
+response=$(curl -X OPTIONS -s "$url")
+
+# Extract the Allow header from the response
+allow_header=$(echo "$response" | grep -i 'allow:')
+
+# Extract the list of allowed methods from the Allow header
+allowed_methods=$(echo "$allow_header" | awk -F':' '{print $2}' | tr -d ' ')
+
+# Display the list of allowed methods
+echo "Allowed methods: $allowed_methods"
